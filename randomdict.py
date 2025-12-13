068a36875f81e2ced749f860a4e819865d53b221
Timon Leupp <timon@leupp.net>
2025-12-13 11:19:11 +0100

    add newest version of RandomDict implementation and update dependencies for scipy

---
+++ randomdict.py
@@ -0,0 +1,85 @@
+"""
+RandomDict
+Source: https://github.com/robtandy/randomdict
+Authors: Rob Tandy & Emery Berger
+License: UNKNOWN
+
+Python dictionaries with O(1) random element access.
+Manually added to the project because this newest version 0.2.1 is not available through PyPi
+"""
+
+try:
+    from collections.abc import MutableMapping
+except ImportError:
+    from collections import MutableMapping
+
+import random
+
+__version__ = '0.2.1'
+
+class RandomDict(MutableMapping):
+    def __init__(self, default_factory=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default_factory = default_factory
+        self.keys = {}
+        self.values = []
+        self.last_index = -1
+
+    def __setitem__(self, key, val):
+        i = self.keys.get(key, -1)
+        if i > -1:
+            self.values[i] = (key, val)
+        else:
+            self.last_index += 1
+            i = self.last_index
+            self.values.append((key, val))
+            self.keys[key] = i
+
+    def __delitem__(self, key):
+        # index of item to delete is i
+        i = self.keys[key]
+        # last item in values array is
+        move_key, move_val = self.values.pop()
+
+        if i != self.last_index:
+            # we move the last item into its location
+            self.values[i] = (move_key, move_val)
+            self.keys[move_key] = i
+        # else it was the last item and we just throw
+        # it away
+
+        # shorten array of values
+        self.last_index -= 1
+        # remove deleted key
+        del self.keys[key]
+
+    def __getitem__(self, key):
+        if key not in self.keys:
+            if self.default_factory is None:
+                raise KeyError(key)
+            self[key] = self.default_factory()
+        i = self.keys[key]
+        return self.values[i][1]
+
+    def __iter__(self):
+        return iter(self.keys)
+
+    def __len__(self):
+        return self.last_index + 1
+
+    def random_key(self):
+        """ Return a random key from this dictionary in O(1) time """
+        if len(self) == 0:
+            raise KeyError("RandomDict is empty")
+
+        i = random.randint(0, self.last_index)
+        return self.values[i][0]
+
+    def random_value(self):
+        """ Return a random value from this dictionary in O(1) time """
+        return self[self.random_key()]
+
+    def random_item(self):
+        """ Return a random key-value pair from this dictionary in O(1) time """
+        k = self.random_key()
+        return k, self[k]
